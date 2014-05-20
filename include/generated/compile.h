6271c4925bfc55cd6ed7a62d74ebff6cec625e9b
corsicanu <corsicanu22@gmail.com>
2014-05-20 10:00:49 +0300

    Initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Tue Mar 12 14:45:28 KST 2013"
+#define LINUX_COMPILE_BY "sophiaeye"
+#define LINUX_COMPILE_HOST "kihyuk"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
